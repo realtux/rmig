74d01a18d8c64e70734a070aa8037bc7d5bcd48f
Brian Seymour <b@bri.io>
2020-05-05 14:47:55 -0500

    dont static compile openssl

--- generate.sh
+++ generate.sh
@@ -2,11 +2,13 @@
 
 # linux deps:
 # none
+echo 'generating for linux...'
 cargo build --release --target x86_64-unknown-linux-gnu
 
 # windows deps:
 # rustup target add x86_64-pc-windows-gnu
 # apt install -y mingw-w64
+echo 'generating for windows...'
 RUSTFLAGS="-C linker=x86_64-w64-mingw32-gcc" \
     cargo build --release --target x86_64-pc-windows-gnu
 
@@ -18,6 +20,7 @@ RUSTFLAGS="-C linker=x86_64-w64-mingw32-gcc" \
 # wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
 # mv MacOSX10.10.sdk.tar.xz tarballs/
 # UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
+echo 'generating for macos...'
 PATH="$HOME/osxcross/target/bin:$PATH" \
 CC=o64-clang \
 CXX=o64-clang++ \
